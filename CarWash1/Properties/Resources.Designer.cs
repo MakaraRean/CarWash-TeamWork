0e75cb4213e219678e012ab3347dc5cf9eff1353
MakaraRean <r.makara232@gmail.com>
2021-11-11 21:50:13 +0700

    kk

--- CarWash1/Properties/Resources.Designer.cs
+++ CarWash1/Properties/Resources.Designer.cs
@@ -123,16 +123,6 @@ namespace CarWash1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ShowPass {
-            get {
-                object obj = ResourceManager.GetObject("ShowPass", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap ShowPass1 {
             get {
                 object obj = ResourceManager.GetObject("ShowPass1", resourceCulture);
