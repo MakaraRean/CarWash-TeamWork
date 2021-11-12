b7265bafc24fc924955bb038362b154e0892c5aa
LoeurngChhay <chhayloeurng02@gmail.com>
2021-11-13 01:21:23 +0700

    Income and Payments

---
+++ CarWash1/Payments.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace CarWash1
+{
+    partial class Payments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Payments));
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotalAmount = new System.Windows.Forms.TextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.txtCashRecieved = new System.Windows.Forms.TextBox();
+            this.txtCashReturn = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtDisPrice = new System.Windows.Forms.TextBox();
+            this.btAddMore = new System.Windows.Forms.Button();
+            this.btPay = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboDiscount = new System.Windows.Forms.ComboBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::CarWash1.Properties.Resources.CarWash;
+            this.pictureBox6.Location = new System.Drawing.Point(198, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(179, 118);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 109;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(77, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 23);
+            this.label4.TabIndex = 108;
+            this.label4.Text = "Cash Return";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(77, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 23);
+            this.label5.TabIndex = 107;
+            this.label5.Text = "Cash Received ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(79, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 23);
+            this.label6.TabIndex = 106;
+            this.label6.Text = "Payment ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(77, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 23);
+            this.label3.TabIndex = 105;
+            this.label3.Text = "Discount Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(79, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 23);
+            this.label2.TabIndex = 104;
+            this.label2.Text = "Discount";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(79, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 23);
+            this.label1.TabIndex = 103;
+            this.label1.Text = "Total Amount";
+            // 
+            // txtTotalAmount
+            // 
+            this.txtTotalAmount.BackColor = System.Drawing.Color.White;
+            this.txtTotalAmount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotalAmount.Location = new System.Drawing.Point(208, 166);
+            this.txtTotalAmount.Name = "txtTotalAmount";
+            this.txtTotalAmount.Size = new System.Drawing.Size(232, 24);
+            this.txtTotalAmount.TabIndex = 102;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(199, 159);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(250, 42);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 101;
+            this.pictureBox5.TabStop = false;
+            // 
+            // txtCashRecieved
+            // 
+            this.txtCashRecieved.BackColor = System.Drawing.Color.White;
+            this.txtCashRecieved.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCashRecieved.Location = new System.Drawing.Point(208, 338);
+            this.txtCashRecieved.Name = "txtCashRecieved";
+            this.txtCashRecieved.Size = new System.Drawing.Size(232, 24);
+            this.txtCashRecieved.TabIndex = 100;
+            // 
+            // txtCashReturn
+            // 
+            this.txtCashReturn.BackColor = System.Drawing.Color.White;
+            this.txtCashReturn.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCashReturn.Location = new System.Drawing.Point(208, 457);
+            this.txtCashReturn.Name = "txtCashReturn";
+            this.txtCashReturn.Size = new System.Drawing.Size(232, 24);
+            this.txtCashReturn.TabIndex = 98;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(199, 450);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 42);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 97;
+            this.pictureBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(208, 398);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 24);
+            this.textBox2.TabIndex = 96;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(199, 391);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 42);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 95;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtDisPrice
+            // 
+            this.txtDisPrice.BackColor = System.Drawing.Color.White;
+            this.txtDisPrice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDisPrice.Location = new System.Drawing.Point(208, 281);
+            this.txtDisPrice.Name = "txtDisPrice";
+            this.txtDisPrice.Size = new System.Drawing.Size(232, 24);
+            this.txtDisPrice.TabIndex = 94;
+            // 
+            // btAddMore
+            // 
+            this.btAddMore.BackColor = System.Drawing.Color.Transparent;
+            this.btAddMore.FlatAppearance.BorderSize = 0;
+            this.btAddMore.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btAddMore.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btAddMore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAddMore.Font = new System.Drawing.Font("Eras Bold ITC", 12F);
+            this.btAddMore.ForeColor = System.Drawing.Color.White;
+            this.btAddMore.Location = new System.Drawing.Point(218, 528);
+            this.btAddMore.Name = "btAddMore";
+            this.btAddMore.Size = new System.Drawing.Size(125, 30);
+            this.btAddMore.TabIndex = 93;
+            this.btAddMore.Text = "Add More";
+            this.btAddMore.UseVisualStyleBackColor = false;
+            // 
+            // btPay
+            // 
+            this.btPay.BackColor = System.Drawing.Color.Transparent;
+            this.btPay.FlatAppearance.BorderSize = 0;
+            this.btPay.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btPay.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btPay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btPay.Font = new System.Drawing.Font("Eras Bold ITC", 12F);
+            this.btPay.ForeColor = System.Drawing.Color.Red;
+            this.btPay.Location = new System.Drawing.Point(363, 528);
+            this.btPay.Name = "btPay";
+            this.btPay.Size = new System.Drawing.Size(88, 30);
+            this.btPay.TabIndex = 92;
+            this.btPay.Text = "Pay";
+            this.btPay.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(199, 274);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 91;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboDiscount
+            // 
+            this.comboDiscount.BackColor = System.Drawing.Color.White;
+            this.comboDiscount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboDiscount.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboDiscount.FormattingEnabled = true;
+            this.comboDiscount.Location = new System.Drawing.Point(202, 219);
+            this.comboDiscount.Name = "comboDiscount";
+            this.comboDiscount.Size = new System.Drawing.Size(240, 31);
+            this.comboDiscount.TabIndex = 89;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(199, 331);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(250, 42);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 90;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(199, 215);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(250, 42);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 99;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Payments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(534, 621);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotalAmount);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.txtCashRecieved);
+            this.Controls.Add(this.txtCashReturn);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtDisPrice);
+            this.Controls.Add(this.btAddMore);
+            this.Controls.Add(this.btPay);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboDiscount);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox4);
+            this.Font = new System.Drawing.Font("Calibri", 14.25F);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Payments";
+            this.Text = "Payments";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalAmount;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox txtCashRecieved;
+        private System.Windows.Forms.TextBox txtCashReturn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtDisPrice;
+        private System.Windows.Forms.Button btAddMore;
+        private System.Windows.Forms.Button btPay;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboDiscount;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
